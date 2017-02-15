205fc32d9dc9381a46df3f1879083d3487a7c1f1
ada mancini <adab3ta@gmail.com>
2017-02-15 14:34:15 -0500

    Remove some settings from nfs::mkdir that are breaking my env

--- manifests/mkdir.pp
+++ manifests/mkdir.pp
@@ -7,15 +7,11 @@ define nfs::mkdir (
   exec { "mkdir_recurse_${name}":
     path    => ['/bin', '/usr/bin'],
     command => "mkdir -p ${name}",
-    unless  => "test -d ${name}",
   }
 
   file { $name:
     ensure  => directory,
     require => Exec["mkdir_recurse_${name}"],
-    mode    => $perm,
-    owner   => $owner,
-    group   => $group,
   }
 
 }
