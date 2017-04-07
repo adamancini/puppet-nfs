7e6c15cabf458e78a77562373bf6524ec9738b41
Ada Mancini <ada@jbanetwork.com>
2017-04-06 20:47:12 -0400

    We don't use augeas

--- manifests/server/ubuntu/service.pp
+++ manifests/server/ubuntu/service.pp
@@ -1,16 +1,7 @@
 class nfs::server::ubuntu::service {
 
   if $nfs::server::ubuntu::service_manage {
-    if $nfs::server::ubuntu::nfs_v4 == true {
-      service {'nfs-kernel-server':
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-        require    => Package['nfs-kernel-server'],
-        subscribe  => [ Concat['/etc/exports'], Augeas['/etc/idmapd.conf','/etc/default/nfs-common'] ],
-      }
-    } else {
+
       service {'nfs-kernel-server':
         ensure     => running,
         enable     => true,
@@ -19,7 +10,7 @@ class nfs::server::ubuntu::service {
         require    => Package['nfs-kernel-server'],
         subscribe  => Concat['/etc/exports'],
       }
-    }
+
 
     Package['rpcbind'] -> Service['rpcbind']
   }
